5e184d8a1c0274688cf7ab6569a6f1d69ff26397
김지형 <112091397+kjh01@users.noreply.github.com>
2024-05-21 05:33:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -14,3 +14,21 @@ features = 3
 # 3차원 테스트 데이터(x,y) 불러오기
 x_test = pd.read_csv()
 y_test = pd.read_csv()
+
+# Load Train Model
+model = load_model('01.Research/02.Preprocessing02/Saved_models/model_in/model_in_NG/model150_256_200.h5')
+np.set_printoptions(precision=10)
+yhat = model.predict(x_test)
+
+# Test Results: df 변환 및 저장
+def list_to_dataframe(matrix):
+    df = pd.DataFrame(matrix, columns=['Lat', 'Lon', 'Time'])
+    return df
+my_ytest = list_to_dataframe(y_test)
+my_yhat = list_to_dataframe(yhat)
+
+# Save Yhat and y_test
+save_path1 = "01.Research/02.Preprocessing02/Ydata_ytest_yhat/ytest_1m/ytest_in_NG/y_test150_c_256_ep200.csv"
+save_path2 = "01.Research/02.Preprocessing02/Ydata_ytest_yhat/yhat_1m/yhat_in_NG/yhat150_c_256_ep200.csv"
+my_ytest.to_csv(save_path1, index=False)
+my_yhat.to_csv(save_path2, index=False)
