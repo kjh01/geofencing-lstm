0f0757799c046233e17581bf17350c6dc9198825
김지형 <112091397+kjh01@users.noreply.github.com>
2024-05-21 16:58:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,7 @@
 - lstm_model.py: 전처리한 학습 데이터 셋 입력> 학습 모델 생성 및 저장.
 - test.py: 전처리한 테스트 데이터 셋 입력> 생성된 학습 모델로 예측 및 결과 저장
 - results.py: 저장한 결과 plot 및 오차 거리 계산
-'''
 
-'''
 parameters = {
     path = "p",     # c / k / m / p
     YN = "out",     # in / out : 테스트 데이터셋이 훈련 데이터셋에 포함되었는지에 대한 유무 확인
@@ -18,3 +16,13 @@ parameters = {
     window_size = 4
 }
 '''
+
+import lstm_model
+import results
+import filter
+
+path = "p"     # c / k / m / p
+YN = "out"     # in / out : 테스트 데이터셋이 훈련 데이터셋에 포함되었는지에 대한 유무 확인
+g = "G"        # G / NG
+features = 3
+window_size = 4
